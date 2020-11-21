54cafc258ade39c18c1a68974f9799bd48319fbb
RamBarath B <49070809+Ram-Barath@users.noreply.github.com>
2020-11-21 11:45:53 +0530

    Initial commit #1

---
+++ whatsapp.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+driver= webdriver.Chrome()
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
+name=input("Enter Name:")
+msg=input("Enter Message:")
+count=int(input("Enter Count:"))
+input("Enter anything after scanning QR code")
+user=driver.find_element_by_xpath("//span[@title='{}']".format(name))
+user.click()
+msg_box = driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[2]/div/div[2]")
+for index in range(count):
+	msg_box.send_keys(msg)
+	y=driver.find_element_by_xpath("//*[@id='main']/footer/div[1]/div[3]/button")
+	y.click()
+print("Success")
